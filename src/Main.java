d231ef9762a6a0f101812ce292f3cafe12d1f0e6
Ernazar <shainazarov.er@gmail.com>
2022-11-18 07:36:43 +0600

    new projeck

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setName("123");
+        System.out.println(animal.getName());
+    }
+}
